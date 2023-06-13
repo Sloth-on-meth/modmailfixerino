49f751bdbdfe7e954361b021f0aaccf6895ff996
sam burkels <sburkels@cracktop.fritz.box>
2023-06-13 22:07:36 +0200

    fuck it

--- modmailcleanup.py
+++ modmailcleanup.py
@@ -1,40 +1,43 @@
 import praw
-import time
+from datetime import datetime, timedelta
 from config import reddit
-#based on https://www.reddit.com/r/ModCoord/comments/147b4n9/heres_a_python_modmail_auto_responder_to/
-# Create a Reddit instance
-import questionary 
 
-sub_name = questionary.ask("What subreddit would you like to run the script on?")
-keywords = ['private', 'blackout', 'dark', 'closed', 'join',  'shut down']
-response_message = "Hello and thank you for your message.  It appears that you are writing in about the Reddit wide blackout to protest API changes. We would like to direct you to [this post](https://www.reddit.com/r/Save3rdPartyApps/comments/1476ioa/reddit_blackout_2023_save_3rd_party_apps/) where you can find, among other information, a list of participating subreddits. \n\nWe will remain closed until further notice, and cannot make exceptions"
+import questionary
+subreddit_name = questionary.text("What sub fam").ask()
+subreddit = reddit.subreddit(subreddit_name )
+modmail_conversations = subreddit.modmail.conversations(state="new")
+
+def process_modmail():
+
+
+# Check if it has been three days
 
 
 
-processed_mail = []
+    for message in subreddit.modmail.conversations():
 
-while True:
-try:
-    print("Fetching modmail conversations...")
-    conversations = reddit.subreddit(sub_name).mod.stream.modmail_conversations(skip_existing=True)
+        if message.unread and message.num_messages < 2:
+            timestamp = (message.last_updated)
+            timestamp_datetime = datetime.fromisoformat(timestamp[:-6])
+            current_datetime = datetime.utcnow()
+            time_difference = current_datetime - timestamp_datetime
+            if time_difference >= timedelta(days=3):
+                continue
+                #print("It has been three days since the timestamp.")
+            else:
+                print(f"replied to {message.authors}")
 
-    for conv in conversations:
-        if len([author for author in conv.authors if author.is_admin]) > 0:
-                reddit.redditor("mod_mailer").message(subject=f"{conv.owner}", message =f"New Admin modmail in r/{conv.owner}\n\n---\n\nNew modmail message from admins https://mod.reddit.com/mail/all/{conv.id}\n\nSubject: {conv.subject}")
-                conv.archive()
+                # Archive the modmail
+            
 
-        if conv.id not in processed_mail:       
-            for message in conv.messages:
-                body = message.body_markdown.lower()
-                if any(keyword in body for keyword in keywords):
-                    print(f"Found modmail in r/{conv.owner} - keyword in message with ID {conv.id} from user {conv.user.name}")
+                # Send a message
+                subject = "We are closed until further notice. Read more -> https://www.reddit.com/r/Save3rdPartyApps/comments/147cksa/why_the_blackouts_happening_from_the_beginning/"
+                body = "Read more https://www.reddit.com/r/Save3rdPartyApps/comments/147cksa/why_the_blackouts_happening_from_the_beginning/"
+                message.reply(subject, body)
+                message.archive()
+                #print(message.body)
 
-                    conv.reply(body=response_message, author_hidden=True)
-                    conv.archive()
-                    processed_mail.append(conv.id)
-                    print(f"Replied to message ID {conv.id} from user {conv.user.name} with the preset response\n")
-                    #print(processed_mail)
-except Exception as e:
-    print(f"An error occurred: {e}")
-    print("Sleeping for 60 seconds before retrying...")
-    time.sleep(60)
+# Run the function to process modmail
+
+
+process_modmail()
